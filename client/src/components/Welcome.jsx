73bd901b75e1fb4af315019cd5a7c9bbe12383e2
anhtudo <doanhtu@cryptopie-labo.com>
2022-04-13 15:18:38 +0700

    chore: add config

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -3,7 +3,8 @@ import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
-import { shortenAddress } from "../ultis/shortenAddress";
+// import { TransactionContext } from "../context/TransactionContext";
+import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from ".";
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -23,7 +24,7 @@ const Welcome = () => {
   // const { currentAccount, connectWallet, handleChange, sendTransaction, formData, isLoading } = useContext(TransactionContext);
 
   const handleSubmit = (e) => {
-    const { addressTo, amount, keyword, message } = formData;
+    // const { addressTo, amount, keyword, message } = formData;
 
     e.preventDefault();
 
@@ -34,7 +35,7 @@ const Welcome = () => {
 
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
         <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
@@ -42,10 +43,10 @@ const Welcome = () => {
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
           </p>
-          {!false && (
+          {/* {!currentAccount && (
             <button
               type="button"
-              // onClick={connectWallet}
+              onClick={connectWallet}
               className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
             >
               <AiFillPlayCircle className="text-white mr-2" />
@@ -53,7 +54,7 @@ const Welcome = () => {
                 Connect Wallet
               </p>
             </button>
-          )}
+          )} */}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
@@ -100,7 +101,7 @@ const Welcome = () => {
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-            {false
+            {/* {isLoading
               ? <Loader />
               : (
                 <button
@@ -110,7 +111,7 @@ const Welcome = () => {
                 >
                   Send now
                 </button>
-              )}
+              )} */}
           </div>
         </div>
       </div>
